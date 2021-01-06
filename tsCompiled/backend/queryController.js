9dafcfe802a7368644076a8bcad7ec70003f2a33
chrisak47 <72534276+chrisak47@users.noreply.github.com>
2021-01-06 03:10:21 -0500

    re-built

--- tsCompiled/backend/queryController.js
+++ tsCompiled/backend/queryController.js
@@ -36,7 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var db = require('../models');
+var db = require('./models');
 var fetch = require('node-fetch');
 var Pool = require('pg').Pool;
 var queryController = {
